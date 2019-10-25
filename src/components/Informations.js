c7464c6000a21240065f6086aca395ef3b5e2761
Jefferson Barros <jeffersonbds_barros@hotmail.com>
2019-10-25 00:34:32 -0300

    Tela informacoes

--- src/components/Informations.js
+++ src/components/Informations.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, ScrollView, SafeAreaView} from 'react-native';
+import {StyleSheet, Text, Image, View, TextInput, TouchableOpacity} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Actions} from 'react-native-router-flux';
@@ -7,19 +7,86 @@ import {Actions} from 'react-native-router-flux';
 const Informations: () => React$Node = () => {
   return (
     <>
-      <SafeAreaView style={{flex: 1}}>
-        <ScrollView>
+    <View style={styles.container}>
 
-          <Text> Informations </Text>
+      <View style={{paddingBottom: 25}}>
+        <Image
+          style={{width: 182, height: 82}}
+          source={require('../images/logo.png')}
+        />
+      </View>
 
-        </ScrollView>
-      </SafeAreaView>
+      <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Insira a marca do seu veículo"
+          placeholderTextColor="#999"
+          style={styles.input}
+          value={1}
+          onChangeText={texto => {return false}}
+      />
+
+      <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Insira o modelo do seu veículo"
+          placeholderTextColor="#999"
+          style={styles.input}
+          value={1}
+          onChangeText={texto => {return false}}
+      />
+
+      <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Média de KM rodados por mês"
+          placeholderTextColor="#999"
+          style={styles.input}
+          value={1}
+          onChangeText={texto => {return false}}
+      />
+
+      <TouchableOpacity onPress={() => {return false}} style={styles.button}>
+          <Text style={styles.buttonText}> Continuar </Text>
+      </TouchableOpacity>
+
+    </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#E8E8E8',
+    padding: 30
+  },
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15
+  },
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#FF4500',
+    borderRadius: 4,
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: 'bold',
+    fontSize: 16
+  }
 });
 
 export default Informations;
